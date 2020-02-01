e2d6d8fd26d72e4872842da5e761d7a3dc14b7e5
Sohrab Roohi <sohrabroohi@ufl.edu>
2020-02-01 01:38:18 -0500

    Line moves horizontally

--- Quick-Shop/src/app/components/map/map.component.ts
+++ Quick-Shop/src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { I18nSelectPipe } from '@angular/common';
 
 @Component({
   selector: 'app-map',
@@ -10,19 +11,40 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    let canvas = <HTMLCanvasElement> document.getElementById("canvas");
+    this.hello();
+
+  }
+
+ async hello() {
+    let canvas = <HTMLCanvasElement>document.getElementById("canvas");
     let ctx = canvas.getContext("2d");
-    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.width = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
-    let img = new Image();
-    img.src = "../../../assets/images/layout.jpg";
-    img.width = window.innerWidth;
-    img.height = window.innerHeight;
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0);
-      let imgData = ctx.getImageData(0, 0, img.width, img.height).data;
-      console.log(imgData);
+    for (var i = 0; i < 4*1000; i += 4) {
+      let img = new Image();
+      img.src = "../../../assets/images/layout.jpg";
+      img.width = window.innerWidth;
+      img.height = window.innerHeight;
+      img.onload = () => {
+        ctx.clearRect(0,0,img.width,img.height);
+        ctx.drawImage(img, 0, 0);
+        let imgData = ctx.getImageData(0, 0, img.width, img.height);
+        for(var j = 0; j < img.height; j++) {
+          imgData.data[i+j*4*img.width+0] = 0;
+          imgData.data[i+j*4*img.width+1] = 0;
+          imgData.data[i+j*4*img.width+2] = 0;
+          imgData.data[i+j*4*img.width+3] = 255;
+        }
+        ctx.clearRect(0,0,img.width,img.height);
+        ctx.putImageData(imgData, 0, 0);
+        (<HTMLImageElement>document.getElementById("myimage")).src = canvas.toDataURL();
+      }
+      await this.delay(1);
     }
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
   }
 
 }

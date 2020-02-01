412cb218d8d6f6927b123e92ec7e1b29faf37b2d
yadiqian <y.qian@ufl.edu>
2020-02-01 10:34:27 -0500

    Update socket server

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -2,13 +2,46 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.emit('message', { from: 'sohrab', content: 'yo'});
+  var x = 950, y = 630;
+  var a = 1200, b = 600;
+  
+  setInterval(function() {
+	var msg = {
+	  me: {
+		x: x,
+		y: y,
+	  },
+	  others: [
+		{
+		  x: 800, 
+		  y: 300
+		},
+		{
+		  x: a, 
+		  y: 400
+		},
+		{
+		  x: 320, 
+		  y: 150
+		},
+		{
+		  x: 820, 
+		  y: 100
+		},
+		{
+		  x: 900, 
+		  y: b
+		}
+	  ]
+	};
+	socket.emit('message', msg);
+	x -= 1;
+	y -= 1;
+	a -= 1;
+	b -= 1;
+  }, 100);
 });
 
 http.listen(3000, function(){

a7d21b66eec60b107474f96f68014a17c6c914f5
yadiqian <y.qian@ufl.edu>
2020-02-01 00:57:22 -0500

    Load image and convert to array

--- Quick-Shop/src/app/components/map/map.component.ts
+++ Quick-Shop/src/app/components/map/map.component.ts
@@ -10,6 +10,19 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let canvas = <HTMLCanvasElement> document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    let img = new Image();
+    img.src = "../../../assets/images/layout.jpg";
+    img.width = window.innerWidth;
+    img.height = window.innerHeight;
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0);
+      let imgData = ctx.getImageData(0, 0, img.width, img.height).data;
+      console.log(imgData);
+    }
   }
 
 }

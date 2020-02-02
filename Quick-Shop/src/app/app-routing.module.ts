64b774b31405e2676fbcf1a0485e3600f1c3fffb
Sohrab Roohi <sohrabroohi@ufl.edu>
2020-02-01 22:18:54 -0500

    Added AI

--- Quick-Shop/src/app/app-routing.module.ts
+++ Quick-Shop/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { MapComponent } from './components/map/map.component';
+import {OptimalComponent} from './optimal/optimal.component';
 
 const routes: Routes = [
   { path: '', component: MapComponent, pathMatch: 'full' },
+  { path: 'optimal', component: OptimalComponent},
   { path: '**', redirectTo: ''}
 ];
 
--- Quick-Shop/src/app/app.module.ts
+++ Quick-Shop/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
+import { OptimalComponent } from './optimal/optimal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    OptimalComponent
   ],
   imports: [
     BrowserModule,
